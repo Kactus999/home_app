8a310bafbb1f0e1c436e70c891e58674dc5bb8de
Kien Tran Tuan <intern.ttkien1@tma.com.vn>
2023-11-29 15:00:24 +0700

    added sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { ProfileComponent } from './modules/profile/profile.component';
+import { AboutComponent } from './modules/about/about.component';
+import { HelpPageComponent } from './modules/help-page/help-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Home' },
+  { path: 'profile', component: ProfileComponent, title: 'Profile' },
+  { path: 'about', component: AboutComponent, title: 'About' },
+  { path: 'help', component: HelpPageComponent, title: 'Help' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './modules/sidebar/sidebar.component';
+import { HomeComponent } from './modules/home/home.component';
+import { ProfileComponent } from './modules/profile/profile.component';
+import { AboutComponent } from './modules/about/about.component';
+import { HelpPageComponent } from './modules/help-page/help-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    ProfileComponent,
+    AboutComponent,
+    HelpPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
